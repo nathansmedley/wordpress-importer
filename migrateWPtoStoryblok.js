d7816912c41fd30444b6fb9813db35b1f0b755cb
nathansmedley <67465787+nathansmedley@users.noreply.github.com>
2025-01-14 10:35:42 +0000

    Update migrateWPtoStoryblok.js

--- migrateWPtoStoryblok.js
+++ migrateWPtoStoryblok.js
@@ -1,26 +1,26 @@
-import {Wp2Storyblok} from './index.js'
- 
+import { Wp2Storyblok } from './index.js'
+
 const wp2storyblok = new Wp2Storyblok('https://voicenationstaging.info/wp-json', {
-  token: 'chmKNZ53u2SZaNrmCPIHewtt-137083-bGPPcQuUwwPbKds5Xxj4',
-  space_id: 1019328,
+  token: 'j9KimjIkcwGGiL7QeXnwdwtt-137083-RTMsxG-zdoEFDpx5yvrU',
+  space_id: 1019557,
   region: 'us',
   content_types: [
     {
-        name: 'posts', // Post type name in WP
-        new_content_type: 'post', // Content Type name in Storyblok
-        folder: 'resources', // Destination folder name in Storyblok
-        schema_mapping: {
-            // Mapping of WP fields to Storyblok fields
-            // WP field name: Storyblok field name
-            "": "content.industry",
-            "": "content.business_size",
-            "title": "name",
-            "": "content.subtitle",
-            "_links.wp:featuredmedia.0": "content.image",
-            "": "content",
-        }          
+      name: 'posts', // Post type name in WP
+      new_content_type: 'article', // Content Type name in Storyblok
+      folder: 'resources/blog', // Destination folder name in Storyblok
+      schema_mapping: {
+        // Mapping of WP fields to Storyblok fields
+        // WPfield: Storyblokfield
+        "": "content.industry",
+        "": "content.business_size",
+        "title": "name",
+        "": "content.subtitle",
+        "_links.wp:featuredmedia.0": "content.image",
+        "": "content",
+      }
     },
   ]
 })
- 
+
 wp2storyblok.migrate()
